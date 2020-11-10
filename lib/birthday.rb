7c6a989ac48f87339b1ecf02affbb8d41ec3ae72
Grant Hesketh <ghesketh76@gmail.com>
2020-11-10 18:43:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,7 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
   
   birthday_kids.each do |key, value|
-    puts
-  
+    puts "Happy Birthday #{key}! You are now #{age} years old!"
+  end
+   
 end
